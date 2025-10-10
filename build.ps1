77949bc31b387bd6245b4a467f7c71b957c7d0be
fezcode <samil.bulbul@gmail.com>
2025-10-10 21:02:18 +0300

    build: powershell build script

---
+++ build.ps1
@@ -0,0 +1,4 @@
+# This creates Windows executables
+$env:GOOS="windows"; $env:GOARCH="amd64"; go build -o ./bin/villian_couch_amd64.exe ./agent/src/
+$env:GOOS="windows"; $env:GOARCH="arm64"; go build -o ./bin/villian_couch_arm64.exe ./agent/src/
+$env:GOOS="windows"; $env:GOARCH="386";   go build -o ./bin/villian_couch_32bit.exe ./agent/src/
